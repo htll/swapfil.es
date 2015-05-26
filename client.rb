0461667bc60b19073db27658675588177575adfa
S0lll0s <s0lll0s@blinkenshell.org>
2015-05-27 00:28:58 +0200

    PoC

--- client.rb
+++ client.rb
@@ -1,26 +1,46 @@
+#!/usr/bin/ruby
+
 require "socket"
-require "thread"
 require "openssl"
 
-host = ARGV.unshift
-port = ARGV.unshift.to_i
+CHUNK_SIZE = 4 * 1024
+
+host = ARGV.shift
+port = ARGV.shift.to_i
 
 #expectedCert = OpenSSL::X509::Certificate.new(File.open("cert.pem"))
-ssl = OpenSSL::SSL::SSLSocket.new TCPSocket.new(host, port)
-ssl.sync_close = true
-ssl.connect
+# ssl = OpenSSL::SSL::SSLSocket.new TCPSocket.new(host, port)
+# ssl.sync_close = true
+# ssl.connect
+ssl = TCPSocket.new host, port
 
 #if ssl.peer_cert.to_s != expectedCert.to_s
 #  stderrr.puts "Unexpected certificate"
 #  exit(1)
 #end
 
-# inp = File.open ARGV.unshift, 'rb'
+inp = File.open ARGV.shift, "rb"
+ssl.puts "#{inp.size}:#{File.basename(inp.path)}"
 
-Thread.new {
-  loop do
-    ssl.write $stdin.read CHUNK_SIZE
-    $stdout.write ssl.read CHUNK_SIZE
-    break if inp.eof? and ssl.eof?
-  end
+size, name = ssl.gets.chomp.split ':', 2
+name = "_" + name while File.exists? name # unique name
+
+out = File.open( name, "wb" )
+print "fetching file '#{name}' (#{size}B)..."
+
+send = Thread.new {
+  ssl.write inp.read CHUNK_SIZE until inp.eof?
 }
+
+recv = Thread.new {
+  size = size.to_i
+  out.write ssl.read [CHUNK_SIZE, size - out.pos].min while out.pos < size
+}
+
+send.join
+recv.join
+
+ssl.close
+out.close
+
+puts "   done!"
--- swapfil.es.rb
+++ swapfil.es.rb
@@ -2,25 +2,49 @@
 
 require "openssl"
 require "socket"
+require "thread"
 
+CHUNK_SIZE = 4 * 1024
 PORT = 6666
 
-ctx = OpenSSL::SSL::SSLContext.new
-ctx.cert = OpenSSL::X509::Certificate.new File.open("cert.pem")
-ctx.key = OpenSSL::PKey::RSA.new File.open("priv.pem")
+# ctx = OpenSSL::SSL::SSLContext.new
+# ctx.cert = OpenSSL::X509::Certificate.new File.open("cert.pem")
+# ctx.key = OpenSSL::PKey::RSA.new File.open("priv.pem")
 
-server = OpenSSL::SSL::SSLServer.new TCPServer.new(PORT), ctx
+# server = OpenSSL::SSL::SSLServer.new TCPServer.new(PORT), ctx
+server = TCPServer.new PORT
 
-puts "Listening on port #{listeningPort}"
+puts "Listening on port #{PORT}"
+
+connections = Hash.new
 
 loop do
-  connection = sslServer.accept
-  Thread.new {
-    # wait for two connections (a, b)
-    # loop do
-      # a.write b.read CHUNK_SIZE
-      # b.write a.read CHUNK_SIZE
-      # break if a.eof? and b.eof?
-    # end
-  }
+  conn = server.accept
+  size, name = conn.gets.chomp.split ":", 2
+  size = size.to_i
+
+  scale = Math.log(size).round
+  puts "Got file '#{name}' of scale #{scale}"
+
+  if connections.has_key? scale
+    Thread.new {
+      a = conn
+      b, b_size, b_name = connections.delete scale
+      b.puts "#{size}:#{name}"
+      a.puts "#{b_size}:#{b_name}"
+
+      loop do
+        a.write b.read [CHUNK_SIZE, b_size].min
+        b.write a.read [CHUNK_SIZE, size].min
+
+        b_size -= [CHUNK_SIZE, b_size].min
+        size -= [CHUNK_SIZE, size].min
+        break if b_size == 0 and size == 0
+      end
+      a.close
+      b.close
+    }
+  else
+    connections[scale] = [conn, size, name]
+  end
 end
