ca97daf70744553eabb2ec779e93b528c160e205
S0lll0s <s0lll0s@blinkenshell.org>
2015-05-28 00:52:16 +0200

    move metadata into TCPSocket info

--- server.rb
+++ server.rb
@@ -19,13 +19,32 @@ puts "Listening on port #{PORT}"
 
 connections = Hash.new
 
+class TCPSocket
+  attr_accessor :name, :size, :scale
+
+  def init
+    @size, @name = self.gets.chomp.split ":", 2
+
+    if !/^\d+$/.match @size or @size.to_i == 0
+      self.puts "ERROR invalid filesize '#{@size}'"
+      self.close
+      throw "ERROR"
+    end
+
+    @size = @size.to_i
+    @name = @name.gsub /[^0-9A-z.\-]/, '_' # no slashes etc.
+
+    @scale = Math.log(@size).round
+    @scale = 1 # @TODO: debug
+  end
+end
+
 Thread.new {
   loop do
     before = Time.now
 
     # kek
     connections.each do |scale, conn|
-      conn, size, name = conn
       conn.puts "PING"
       begin 
         timeout 2 do
@@ -45,41 +64,29 @@ Thread.new {
 
 loop do
   conn = server.accept
-  size, name = conn.gets.chomp.split ":", 2
+  conn.init
 
-  if !/^\d+$/.match size or size.to_i == 0
-    conn.puts "ERROR invalid filesize '#{size}'"
-    conn.close
-    next
-  end
-
-  size = size.to_i
-  name = name.gsub /[^0-9A-z.\-]/, '_' # no slashes etc.
-
-  scale = Math.log(size).round
-  scale = 1
-
-  if connections.has_key? scale and not connections[scale][0].closed?
+  if connections.has_key? conn.scale and not connections[conn.scale].closed?
     Thread.new {
       a = conn
-      b, b_size, b_name = connections.delete scale
+      b = connections.delete a.scale
       a.puts "GO"
       b.puts "GO"
-      b.puts "#{size}:#{name}"
-      a.puts "#{b_size}:#{b_name}"
+      b.puts "#{a.size}:#{a.name}"
+      a.puts "#{b.size}:#{b.name}"
 
       loop do
-        a.write b.read [CHUNK_SIZE, b_size].min
-        b.write a.read [CHUNK_SIZE, size].min
+        a.write b.read [CHUNK_SIZE, b.size].min
+        b.write a.read [CHUNK_SIZE, a.size].min
 
-        b_size -= [CHUNK_SIZE, b_size].min
-        size -= [CHUNK_SIZE, size].min
-        break if b_size == 0 and size == 0
+        b.size -= [CHUNK_SIZE, b.size].min
+        a.size -= [CHUNK_SIZE, a.size].min
+        break if b.size == 0 and a.size == 0
       end
       a.close
       b.close
     }
   else
-    connections[scale] = [conn, size, name]
+    connections[conn.scale] = conn
   end
 end
